796095386f49311c66732154a4d27f85e0430843
Joshua Estes <Joshua.Estes@iostudio.com>
2012-08-03 12:28:01 -0500

    commiting unfished code to work on later, deal with it

--- Command/Init/ProjectCommand.php
+++ Command/Init/ProjectCommand.php
@@ -7,6 +7,8 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
 
 class ProjectCommand extends ContainerAwareCommand
 {
@@ -26,13 +28,71 @@ EOF
             );
     }
 
+    protected function initialize(InputInterface $input, OutputInterface $ouput)
+    {
+        if (!$input->isInteractive()) {
+            throw new \RuntimeException('This command must be run in interactive mode.');
+        }
+
+        if (!is_file($this->getConfigFile('parameters.yml')) && is_file($this->getConfigFile('parameters.yml.dist'))) {
+            $filesystem = new Filesystem();
+            $filesystem->copy($parametersDistFile, $parametersFile);
+        }
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
     }
 
     protected function interact(InputInterface $input, OutputInterface $output)
     {
+        $dialog    = $this->getDialogHelper();
+        $formatter = $this->getFormatterHelper();
 
+        $output->writeln(array('',$formatter->formatBlock('Biga Initialize Project', 'bg=blue;fg=white', true),''));
+
+        // Secret
+        $output->writeln(array(
+            '',
+            '',
+        ));
+
+        if ($dialog->askConfirmation($output, $this->getQuestion("Do you want to generate a secret", 'Y/n'), true)) {
+            $parameters = Yaml::parse(file_get_contents($this->getConfigFile('parameters.yml')));
+            $parameters['parameters'] = array_merge($parameters['parameters'], array(
+                'secret' => md5(uniqid(time(),true)),
+            ));
+            file_put_contents($this->getConfigFile('parameters.yml'), Yaml::dump($parameters));
+        }
+
+        // Locale
+        $output->writeln(array(
+            '',
+            '',
+        ));
+
+        if ($dialog->askConfirmation($output, $this->getQuestion("Locale", 'en'), 'en')) {
+        }
+    }
+
+    protected function getDialogHelper()
+    {
+        return $this->getHelperSet()->get('dialog');
+    }
+
+    protected function getFormatterHelper()
+    {
+        return $this->getHelperSet()->get('formatter');
+    }
+
+    protected function getQuestion($question, $default)
+    {
+        return sprintf('<info>%s</info> [<comment>%s</comment>]: ', $question, $default);
+    }
+
+    protected function getConfigFile($file)
+    {
+        return $this->getContainer()->getParameter('kernel.root_dir') . '/config/' . $file;
     }
 
 }
