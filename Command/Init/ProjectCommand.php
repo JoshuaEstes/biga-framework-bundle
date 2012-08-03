0d111fb2c571d503ed812d39e069e02b39ee08b1
Joshua Estes <Joshua.Estes@iostudio.com>
2012-08-03 09:41:29 -0500

    Added help text to file

--- Command/Init/ProjectCommand.php
+++ Command/Init/ProjectCommand.php
@@ -15,11 +15,23 @@ class ProjectCommand extends ContainerAwareCommand
     {
         $this
             ->setName('init:project')
-            ->setDescription('Initialize a project');
+            ->setDescription('Initialize a project')
+            ->setHelp(<<<EOF
+This command only really needs to be run once. After that
+everything should be setup. This command takes care of updating
+your parameters.yml file, adding an entry into your hosts file,
+and creating a vhost entry for you.
+EOF
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+
     }
 
 }
