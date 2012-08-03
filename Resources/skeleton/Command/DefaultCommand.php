8cec7b881c1a4d0b883dab91c93aed65d5dc358f
Joshua Estes <Joshua.Estes@iostudio.com>
2012-08-03 10:51:21 -0500

    added a default command skeleton

---
+++ Resources/skeleton/Command/DefaultCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace %namespace%;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setDefinition(array(
+            ))
+            ->setName('%name%')
+            ->setDescription('%description%')
+            ->setHelp(<<<EOF
+%help%
+EOF
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function initialize(InputInterface $input, OutputInterface $ouput)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+    }
+
+}
