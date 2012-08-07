6b39d13dd287f1d0f3c5de65308ca1d6851eaa02
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2012-08-06 19:06:50 -0500

    updated validators and wrote unit test

--- Command/Validators.php
+++ Command/Validators.php
@@ -7,7 +7,7 @@ class Validators
 
     public static function validateHost($host)
     {
-        if (null === $host) {
+        if (empty($host)) {
             throw new \InvalidArgumentException('Invalid host');
         }
 
@@ -16,57 +16,96 @@ class Validators
 
     public static function validateHostsFile($hosts_file)
     {
+        if (empty($hosts_file)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $hosts_file;
     }
 
     public static function validateIPAddress($ip_address)
     {
+        if (empty($ip_address)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
         return $ip_address;
     }
 
     public static function validatePort($port)
     {
+        if (empty($port)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $port;
     }
 
     public static function validateDocumentRoot($document_root)
     {
+        if (empty($document_root)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $document_root;
     }
 
     public static function validatePriority($priority)
     {
+        if (empty($priority)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $priority;
     }
 
     public static function validateDatabaseDriver($driver)
     {
+        if (empty($driver)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $driver;
     }
 
     public static function validateDatabaseHost($host)
     {
+        if (empty($host)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $host;
     }
 
     public static function validateDatabasePort($port)
     {
+        if (empty($port)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $port;
     }
 
     public static function validateDatabaseName($name)
     {
+        if (empty($name)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $name;
     }
 
     public static function validateDatabaseUsername($username)
     {
+        if (empty($username)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $username;
     }
 
     public static function validateDatabasePassword($password)
     {
-        if (null === $password) {
+        if (empty($password)) {
             throw new \InvalidArgumentException('You must provide a password');
         }
 
@@ -75,26 +114,46 @@ class Validators
 
     public static function validateMailerTransport($transport)
     {
+        if (empty($transport)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $transport;
     }
 
     public static function validateMailerHost($host)
     {
+        if (empty($host)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $host;
     }
 
     public static function validateMailerUsername($username)
     {
+        if (empty($username)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $username;
     }
 
     public static function validateMailerPassword($password)
     {
+        if (empty($password)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $password;
     }
 
     public static function validateLocale($locale)
     {
+        if (empty($locale)) {
+            throw new \InvalidArgumentException('Invalid');
+        }
+
         return $locale;
     }
 }
---
+++ Tests/Command/ValidatorsTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace BigaFrameworkBundle\Tests\Command;
+
+use BigaFrameworkBundle\Command\Validators;
+
+class ValidatorsTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testValidateHost()
+    {
+        $host = 'test.local';
+        $this->assertEquals($host, Validators::validateHost($host));
+    }
+
+    public function testValidateHostsFile()
+    {
+        $hostsFile = '/etc/hosts';
+        $this->assertEquals($hostsFile, Validators::validateHostsFile($hostsFile));
+    }
+
+    public function testValidateIPAddress()
+    {
+        $ip = '127.0.0.1';
+        $this->assertEquals($ip, Validators::validateIPAddress($ip));
+    }
+
+    public function testValidatePort()
+    {
+        $port = '8080';
+        $this->assertEquals($port, Validators::validatePort($port));
+    }
+
+    public function testValidateDocumentRoot()
+    {
+        $docRoot = '/var/www/test';
+        $this->assertEquals($docRoot, Validators::validateDocumentRoot($docRoot));
+    }
+
+    public function testValidatePriority()
+    {
+        $priority = '0000';
+        $this->assertEquals($priority, Validators::validatePriority($priority));
+    }
+
+    public function testValidateDatabaseDriver()
+    {
+        $driver = 'pdo';
+        $this->assertEquals($driver, Validators::validateDatabaseDriver($driver));
+    }
+
+    public function testValidateDatabaseHost()
+    {
+        $this->assertEquals('localhost', Validators::validateDatabaseHost('localhost'));
+    }
+
+    public function testValidateDatabasePort()
+    {
+        $this->assertEquals('3306', Validators::validateDatabasePort('3306'));
+    }
+
+    public function testValidateDatabaseName()
+    {
+        $this->assertEquals('biga', Validators::validateDatabaseName('biga'));
+    }
+
+    public function testValidateDatabaseUsername()
+    {
+        $this->assertEquals('root', Validators::validateDatabaseUsername('root'));
+    }
+
+    public function testValidateDatabasePassword()
+    {
+        $this->assertEquals('root', Validators::validateDatabasePassword('root'));
+    }
+
+    public function testValidateMailerTransport()
+    {
+        $this->assertEquals('sendmail', Validators::validateMailerTransport('sendmail'));
+    }
+
+    public function testValidateMailerHost()
+    {
+        $this->assertEquals('localhost', Validators::validateMailerHost('localhost'));
+    }
+
+    public function testValidateMailerUsername()
+    {
+        $this->assertEquals('root', Validators::validateMailerUsername('root'));
+    }
+
+    public function testValidateMailerPassword()
+    {
+        $this->assertEquals('root', Validators::validateMailerPassword('root'));
+    }
+
+    public function testValidateLocale()
+    {
+        $this->assertEquals('en', Validators::validateLocale('en'));
+    }
+
+}
