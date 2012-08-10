4895e5786c6fc444dfa0af09ad3bc44219679805
Joshua Estes <Joshua.Estes@iostudio.com>
2012-08-10 14:12:55 -0500

    refactored and updated

--- Command/Configure/MailerCommand.php
+++ Command/Configure/MailerCommand.php
@@ -10,10 +10,11 @@
 namespace BigaFrameworkBundle\Command\Configure;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Yaml\Yaml;
 
 /**
@@ -24,12 +25,15 @@ use Symfony\Component\Yaml\Yaml;
 class MailerCommand extends ContainerAwareCommand
 {
 
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
             ->setDefinition(array(
-                new InputOption('transport', '', InputOption::VALUE_REQUIRED, 'Mailer transport', 'mail'),
-                new InputOption('host', '', InputOption::VALUE_REQUIRED, 'Mailer host or IP address'),
+                new InputOption('transport', '', InputOption::VALUE_REQUIRED, 'Mailer transport', 'smtp'),
+                new InputOption('host', '', InputOption::VALUE_REQUIRED, 'Mailer host or IP address', 'localhost'),
                 new InputOption('user', '', InputOption::VALUE_REQUIRED, 'Mailer username'),
                 new InputOption('password', '', InputOption::VALUE_REQUIRED, 'Mailer password'),
             ))
@@ -40,6 +44,9 @@ EOF
             );
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $parametersFile = $this->getContainer()->getParameter('kernel.root_dir') . '/config/parameters.yml';
@@ -57,79 +64,105 @@ EOF
 
         file_put_contents($parametersFile, Yaml::dump($parametersArray));
 
-        $output->writeln('Parameters file has been updated.');
+        $output->writeln(array(
+            '',
+            'Parameters file has been updated.',
+            '',
+        ));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function interact(InputInterface $input, OutputInterface $output)
     {
         $dialog    = $this->getDialogHelper();
-        $formatter = $this->getFormatterHelper();
+        $validator = Validation::createValidator();
 
-        $output->writeln(array('',$formatter->formatBlock('Biga Mailer Configurator', 'bg=blue;fg=white', true),''));
+        $dialog->writeSection($output, 'Biga Mailer Configurator');
 
         // Mailer Transport
         $output->writeln(array(
             '',
             'The mailer transport can be any one of the following:',
             '',
-            '<comment>',
-            'smtp',
-            'gmail',
-            'mail',
-            'sendmail',
-            '</comment>',
+            '<comment>smtp</comment>',
+            '<comment>gmail</comment>',
+            '<comment>mail</comment>',
+            '<comment>sendmail</comment>',
             '',
-            '<info>Some of these transports require a username, password, host, and port.</info>',
+            '<info>NOTE: Some of these transports require a username, password, host, and port.</info>',
             '',
         ));
 
-        $transport = $dialog->askAndValidate($output, $this->getQuestion("Mailer Transport", $input->getOption('transport')), 'BigaFrameworkBundle\\Command\\Validators::validateMailerTransport', false, $input->getOption('transport'));
+        $transport = $dialog->askAndValidate($output, $dialog->getQuestion("Mailer Transport", $input->getOption('transport')), function($value) {
+            if (!in_array($value, array('smtp', 'gmail', 'mail', 'sendmail'))) {
+                throw new \InvalidArgumentException('Invalid transport');
+            }
+            return $value;
+        }, false, $input->getOption('transport'));
         $input->setOption('transport', $transport);
 
         // Mailer Host
         $output->writeln(array(
             '',
-            '',
+            'Hostname or IP Address of the host mailer',
             '',
         ));
 
-        $host = $dialog->askAndValidate($output, $this->getQuestion("Mailer Host", $input->getOption('host')), 'BigaFrameworkBundle\\Command\\Validators::validateMailerHost', false, $input->getOption('host'));
+        $host = $dialog->askAndValidate($output, $dialog->getQuestion("Mailer Host", $input->getOption('host')), function($value) use($validator) {
+            $errors = $validator->validateValue($value, new Assert\NotBlank());
+            if (count($errors)) {
+                throw new \InvalidArgumentException(trim(str_replace(":\n", ":", (string) $errors)));
+            }
+            return $value;
+        }, false, $input->getOption('host'));
         $input->setOption('host', $host);
 
         // Mailer Username
         $output->writeln(array(
             '',
-            '',
+            'Depending upon what transport type you selected this will be required.',
             '',
         ));
 
-        $user = $dialog->askAndValidate($output, $this->getQuestion("Mailer Username", $input->getOption('user')), 'BigaFrameworkBundle\\Command\\Validators::validateMailerUsername', false, $input->getOption('user'));
+        $user = $dialog->askAndValidate($output, $dialog->getQuestion("Mailer Username", $input->getOption('user')), function($value) use($validator, $input) {
+            if (in_array($input->getOption('transport'), array('gmail'))) {
+                $errors = $validator->validateValue($value, new Assert\NotBlank());
+                if (count($errors)) {
+                    throw new \InvalidArgumentException(trim(str_replace(":\n", ":", (string) $errors)));
+                }
+            }
+            return $value;
+        }, false, $input->getOption('user'));
         $input->setOption('user', $user);
 
         // Mailer Password
         $output->writeln(array(
             '',
-            '',
+            'Depending on what transport you selected, this may be required.',
             '',
         ));
 
-        $password = $dialog->askAndValidate($output, $this->getQuestion("Mailer Password", $input->getOption('password')), 'BigaFrameworkBundle\\Command\\Validators::validateMailerPassword', false, $input->getOption('password'));
+        $password = $dialog->askAndValidate($output, $dialog->getQuestion("Mailer Password", $input->getOption('password')), function($value) use($validator, $input) {
+            if (in_array($input->getOption('transport'), array('gmail'))) {
+                $errors = $validator->validateValue($value, new Assert\NotBlank());
+                if (count($errors)) {
+                    throw new \InvalidArgumentException(trim(str_replace(":\n", ":", (string) $errors)));
+                }
+            }
+            return $value;
+        }, false, $input->getOption('password'));
         $input->setOption('password', $password);
     }
 
     protected function getDialogHelper()
     {
-        return $this->getHelperSet()->get('dialog');
-    }
-
-    protected function getFormatterHelper()
-    {
-        return $this->getHelperSet()->get('formatter');
-    }
-
-    protected function getQuestion($question, $default)
-    {
-        return sprintf('<info>%s</info> [<comment>%s</comment>]: ', $question, $default);
+        $dialog = $this->getHelperSet()->get('dialog');
+        if (!$dialog || get_class($dialog) !== 'BigaFrameworkBundle\Helper\DialogHelper') {
+            $this->getHelperSet()->set($dialog = new \BigaFrameworkBundle\Helper\DialogHelper());
+        }
+        return $dialog;
     }
 
 }
